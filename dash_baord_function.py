0b381d763338b710487a123a1002928dea1e2624
anilkumarKanasani <anilkumar.kanasani@gmail.com>
2023-08-11 12:40:02 +0200

    Creadit and Debit are converted to single column as amount

--- dash_baord_function.py
+++ dash_baord_function.py
@@ -2,9 +2,13 @@ def create_dashboard(df):
     import streamlit as st
     import pandas as pd
 
-    kpi_dict = dict(zip(df["Sub-category"], df["amount"]))
-    kpi_percent_dict = dict(zip(df["Sub-category"], df["percentage"]))
+    df = df.round(2)
+
+    kpi = dict(zip(df["Sub-category"], df["amount"]))
+    kpi_perc = dict(zip(df["Sub-category"], df["percentage"]))
+
     st.divider()
+    # Income Category columns
     (
         text_1_col,
         sal_col,
@@ -14,59 +18,44 @@ def create_dashboard(df):
         Total_Income_pdp_col,
         Total_Income_col,
     ) = st.columns(7)
-    total_gross_income = kpi_dict["Salary"] + kpi_dict["Tax less Income"]
-    total_gross_income_per = (
-        kpi_percent_dict["Salary"] + kpi_percent_dict["Tax less Income"]
-    )
+
     text_1_col.subheader("Income Categories")
     sal_col.image("supporting_data/images/Salary.png", width=100)
     sal_col.metric(
         "Salary Income",
-        round(kpi_dict["Salary"], 2),
-        round(kpi_percent_dict["Salary"], 2),
+        kpi["Salary"],
+        kpi_perc["Salary"],
     )
     Tax_less_income_col.image("supporting_data/images/Tax_less_income.png", width=100)
     Tax_less_income_col.metric(
         "Tax less Income",
-        round(kpi_dict["Tax less Income"], 2),
-        round(kpi_percent_dict["Tax less Income"], 2),
+        kpi["Tax less Income"],
+        kpi_perc["Tax less Income"],
     )
 
     PDP_income_col.image("supporting_data/images/PDP_Income.png", width=100)
     PDP_income_col.metric(
         "PDP Income",
-        round(kpi_dict["PDP Income"], 2),
+        kpi["PDP Income"],
         "Excluded from total & percentage calculation",
     )
     Off_travel_income_col.image("supporting_data/images/office_travel.png", width=100)
     Off_travel_income_col.metric(
         "Office Travel Income",
-        round(kpi_dict["Office Travel Income"], 2),
+        kpi["Office Travel Income"],
         "Excluded from total & percentage calculation",
     )
 
     Total_Income_pdp_col.image("supporting_data/images/total_income.png", width=100)
     Total_Income_pdp_col.metric(
         "Total Gross + PDP Income",
-        round(
-            kpi_dict["Salary"]
-            + kpi_dict["Tax less Income"]
-            + kpi_dict["PDP Income"]
-            + kpi_dict["Office Travel Income"],
-            2,
-        ),
+        kpi["total_gross_income_pdp"],
     )
     Total_Income_col.image("supporting_data/images/total_income.png", width=100)
     Total_Income_col.metric(
         "Total Gross Income",
-        round(
-            total_gross_income,
-            2,
-        ),
-        round(
-            total_gross_income_per,
-            2,
-        ),
+        kpi["total_gross_income"],
+        kpi_perc["total_gross_income"],
     )
 
     st.divider()
@@ -86,86 +75,52 @@ def create_dashboard(df):
     Income_Tax_col.image("supporting_data/images/Income_tax.png", width=100)
     Income_Tax_col.metric(
         "Income Tax",
-        round(kpi_dict["Tax"], 2),
-        round(kpi_percent_dict["Tax"], 2),
+        kpi["Tax"],
+        kpi_perc["Tax"],
     )
     Health_Insurance_col.image("supporting_data/images/health_insurance.png", width=100)
     Health_Insurance_col.metric(
         "Health Insurance",
-        round(kpi_dict["Health Insurance"], 2),
-        round(kpi_percent_dict["Health Insurance"], 2),
+        kpi["Health Insurance"],
+        kpi_perc["Health Insurance"],
     )
     Pension_col.image("supporting_data/images/pension.png", width=100)
     Pension_col.metric(
         "Pension",
-        round(kpi_dict["Pension"], 2),
-        round(kpi_percent_dict["Pension"], 2),
+        kpi["Pension"],
+        kpi_perc["Pension"],
     )
     Unemployment_fund_col.image(
         "supporting_data/images/unemplyment_fund.png", width=100
     )
     Unemployment_fund_col.metric(
         "Unemployment Fund",
-        round(kpi_dict["Unemployment Fund"], 2),
-        round(kpi_percent_dict["Unemployment Fund"], 2),
+        kpi["Unemployment Fund"],
+        kpi_perc["Unemployment Fund"],
     )
     Nursing_care_col.image("supporting_data/images/health_insurance.png", width=100)
     Nursing_care_col.metric(
         "Nursing Care",
-        round(kpi_dict["Nursing Care"], 2),
-        round(kpi_percent_dict["Nursing Care"], 2),
+        kpi["Nursing Care"],
+        kpi_perc["Nursing Care"],
     )
-    total_state_cuttings = (
-        kpi_dict["Tax"]
-        + kpi_dict["Health Insurance"]
-        + kpi_dict["Pension"]
-        + kpi_dict["Unemployment Fund"]
-        + kpi_dict["Nursing Care"]
-    )
-    total_state_cuttings_per = (
-        kpi_percent_dict["Tax"]
-        + kpi_percent_dict["Health Insurance"]
-        + kpi_percent_dict["Pension"]
-        + kpi_percent_dict["Unemployment Fund"]
-        + kpi_percent_dict["Nursing Care"]
-    )
+
     Total_state_cut_col.image("supporting_data/images/Total_cuttings.png", width=100)
     Total_state_cut_col.metric(
         "Total State Cuttings",
-        round(
-            total_state_cuttings,
-            2,
-        ),
-        round(
-            total_state_cuttings_per,
-            2,
-        ),
+        kpi["total_state_cuttings"],
+        kpi_perc["total_state_cuttings"],
     )
 
     net_income_col.image("supporting_data/images/net_income.png", width=100)
     net_income_col.metric(
         "Total Net Income",
-        round(total_gross_income - total_state_cuttings, 2),
-        round(total_gross_income_per - total_state_cuttings_per, 2),
+        kpi["total_net_income"],
+        kpi_perc["total_net_income"],
     )
 
     st.divider()
 
-    tot_living_exp = (
-        kpi_dict["Rent"]
-        + kpi_dict["Phone & WiFi"]
-        + kpi_dict["Groceries"]
-        + kpi_dict["Travel Pass"]
-        + kpi_dict["Gifts"]
-    )
-
-    tot_living_exp_per = (
-        kpi_percent_dict["Rent"]
-        + kpi_percent_dict["Phone & WiFi"]
-        + kpi_percent_dict["Groceries"]
-        + kpi_percent_dict["Travel Pass"]
-        + kpi_percent_dict["Gifts"]
-    )
     (
         text_3_col,
         Rent_col,
@@ -180,61 +135,38 @@ def create_dashboard(df):
     Rent_col.image("supporting_data/images/rent.png", width=100)
     Rent_col.metric(
         "Room Rent",
-        round(kpi_dict["Rent"], 2),
-        round(kpi_percent_dict["Rent"], 2),
+        kpi["Rent"],
+        kpi_perc["Rent"],
     )
     Phne_wifi_col.image("supporting_data/images/cell_phone.png", width=100)
     Phne_wifi_col.metric(
         "Phone & WiFi",
-        round(kpi_dict["Phone & WiFi"], 2),
-        round(kpi_percent_dict["Phone & WiFi"], 2),
+        kpi["Phone & WiFi"],
+        kpi_perc["Phone & WiFi"],
     )
     Groceseries_col.image("supporting_data/images/groceries.png", width=100)
     Groceseries_col.metric(
         "Groceseries",
-        round(kpi_dict["Groceries"], 2),
-        round(kpi_percent_dict["Groceries"], 2),
+        kpi["Groceries"],
+        kpi_perc["Groceries"],
     )
     Monthly_travel_col.image("supporting_data/images/Monthly_travel.png", width=100)
     Monthly_travel_col.metric(
         "Monthly Travel",
-        round(kpi_dict["Travel Pass"], 2),
-        round(kpi_percent_dict["Travel Pass"], 2),
+        kpi["Travel Pass"],
+        kpi_perc["Travel Pass"],
     )
     Gifts_col.image("supporting_data/images/unemplyment_fund.png", width=100)
-    Gifts_col.metric(
-        "Gifts", round(kpi_dict["Gifts"], 2), round(kpi_percent_dict["Gifts"], 2)
-    )
+    Gifts_col.metric("Gifts", kpi["Gifts"], kpi_perc["Gifts"])
     Total_living_exp_col.image("supporting_data/images/total_living.png", width=100)
     Total_living_exp_col.metric(
         "Total Living Expenses",
-        round(tot_living_exp, 2),
-        round(tot_living_exp_per, 2),
+        kpi["tot_living_exp"],
+        kpi_perc["tot_living_exp"],
     )
 
     st.divider()
 
-    tot_extra_exp = (
-        kpi_dict["Entertainment"]
-        + kpi_dict["Vacation"]
-        + kpi_dict["Furnishings"]
-        + kpi_dict["Family & Kids"]
-        + kpi_dict["Restaurant"]
-        + kpi_dict["Taxi"]
-        + kpi_dict["Cloths"]
-        + kpi_dict["Gym & Self grooming"]
-    )
-
-    tot_extra_exp_per = (
-        kpi_percent_dict["Entertainment"]
-        + kpi_percent_dict["Vacation"]
-        + kpi_percent_dict["Furnishings"]
-        + kpi_percent_dict["Family & Kids"]
-        + kpi_percent_dict["Restaurant"]
-        + kpi_percent_dict["Taxi"]
-        + kpi_percent_dict["Cloths"]
-        + kpi_percent_dict["Gym & Self grooming"]
-    )
     (
         text_4_col,
         Entertainment_col,
@@ -248,31 +180,29 @@ def create_dashboard(df):
     text_4_col.subheader("Extra Expenses Categories")
     Entertainment_col.metric(
         "Entertainment & Vacation",
-        round(kpi_dict["Entertainment"] + kpi_dict["Vacation"], 2),
-        round(kpi_percent_dict["Entertainment"] + kpi_percent_dict["Vacation"], 2),
+        kpi["Entertainment"] + kpi["Vacation"],
+        kpi_perc["Entertainment"] + kpi_perc["Vacation"],
     )
     Furnishings_col.metric(
         "Furnishings",
-        round(kpi_dict["Furnishings"], 2),
-        round(kpi_percent_dict["Furnishings"], 2),
+        kpi["Furnishings"],
+        kpi_perc["Furnishings"],
     )
     Family_Kids_col.metric(
         "Family & Kids",
-        round(kpi_dict["Family & Kids"], 2),
-        round(kpi_percent_dict["Family & Kids"], 2),
+        kpi["Family & Kids"],
+        kpi_perc["Family & Kids"],
     )
     Restaurant_col.metric(
         "Restaurant",
-        round(kpi_dict["Restaurant"], 2),
-        round(kpi_percent_dict["Restaurant"], 2),
+        kpi["Restaurant"],
+        kpi_perc["Restaurant"],
     )
-    Taxi_col.metric(
-        "Taxi", round(kpi_dict["Taxi"], 2), round(kpi_percent_dict["Taxi"], 2)
-    )
+    Taxi_col.metric("Taxi", kpi["Taxi"], kpi_perc["Taxi"])
     empt_2_col.image("supporting_data/images/total_living_extra.png", width=100)
 
     (
-        empt_3_col,
+        empt_col,
         Cloths_col,
         gym_col,
         pdp_spent_col,
@@ -281,30 +211,32 @@ def create_dashboard(df):
         tot_expenses_col,
     ) = st.columns(7)
 
-    Cloths_col.metric("Cloths", round(kpi_dict["Cloths"], 2))
+    Cloths_col.metric("Cloths", kpi["Cloths"])
     gym_col.metric(
         "Gym & Self grooming",
-        round(kpi_dict["Gym & Self grooming"], 2),
-        round(kpi_percent_dict["Gym & Self grooming"], 2),
+        kpi["Gym & Self grooming"],
+        kpi_perc["Gym & Self grooming"],
     )
     pdp_spent_col.metric(
         "PDP Spending",
-        round(kpi_dict["PDP Spending"], 2),
+        kpi["PDP Spending"],
         "Excluded from total & percentage calculation",
     )
     office_travel_spent_col.metric(
         "Office Travel Spending",
-        round(kpi_dict["Office Travel Spending"], 2),
+        kpi["Office Travel Spending"],
         "Excluded from total & percentage calculation",
     )
     tot_extra_expenses_col.metric(
-        "Total Extra Expenses", round(tot_extra_exp, 2), round(tot_extra_exp_per, 2)
+        "Total Extra Expenses",
+        kpi["tot_extra_exp"],
+        kpi_perc["tot_extra_exp"],
     )
 
     tot_expenses_col.metric(
         "Total Living & Extra Expenses",
-        round(tot_living_exp + tot_extra_exp, 2),
-        round(tot_living_exp_per + tot_extra_exp_per, 2),
+        kpi["complete_expenses"],
+        kpi_perc["complete_expenses"],
     )
 
     st.divider()
@@ -313,9 +245,9 @@ def create_dashboard(df):
         text_5_col,
         Ind_savings_col,
         Europe_savings_col,
-        empt_2_col,
-        empt_3_col,
-        empt_4_col,
+        empt_col,
+        empt_col,
+        empt_col,
         tot_savings_col,
     ) = st.columns(7)
 
@@ -323,20 +255,20 @@ def create_dashboard(df):
     Ind_savings_col.image("supporting_data/images/India_savings.png", width=100)
     Ind_savings_col.metric(
         "India Savings & Expenses",
-        round(kpi_dict["To India"], 2),
-        round(kpi_percent_dict["To India"], 2),
+        kpi["To India"],
+        kpi_perc["To India"],
     )
     Europe_savings_col.image("supporting_data/images/Europe_savings.png", width=100)
     Europe_savings_col.metric(
         "Europe Long term savings",
-        round(kpi_dict["Company Pension"], 2),
-        round(kpi_percent_dict["Company Pension"], 2),
+        kpi["Company Pension"],
+        kpi_perc["Company Pension"],
     )
 
     tot_savings_col.image("supporting_data/images/savings.png", width=100)
     tot_savings_col.metric(
         "Total savings",
-        round(kpi_dict["To India"] + kpi_dict["Company Pension"], 2),
-        round(kpi_percent_dict["To India"] + kpi_percent_dict["Company Pension"], 2),
+        kpi["total_savings"],
+        kpi_perc["total_savings"],
     )
     st.divider()
