071c7b3c1cfb4709f17d88b4494861389aa44881
anilkumarKanasani <anilkumar.kanasani@gmail.com>
2023-10-24 12:26:53 +0200

    Helper function is optimized to created cc bill processing csv file

--- healper_functions.py
+++ healper_functions.py
@@ -1,14 +1,22 @@
 def transform_cc_bill():
     import pandas as pd
-    df = pd.read_csv('~/Downloads/aktivitäten.csv', sep=';')
+    df = pd.read_csv('~/Downloads/activity.csv', sep=';')
     # reverse the order of the rows
     df = df.iloc[::-1]
     # replace / with . in Datum column
     df['Datum'] = df['Datum'].str.replace('/', '.').str.replace('2023', '23')
+    # extract first word from Beschreibung
+    df['Beschreibung'] = df['Beschreibung'].str.split().str[0]
     # replace , with . in Betrag column and covert to negative
     df['Betrag'] = df['Betrag'].str.replace(',', '.').astype(float)* -1
+    # rename column names
+    df.rename(columns={'Beschreibung': 'Description',
+                       'Betrag': 'amount'}, inplace=True)
+    df['Sub-category'] = "  "
+    # re order the columns
+    df = df[['Datum', 'Description', 'Sub-category', 'amount']]
     df.to_csv('~/Downloads/processed.csv', sep=';')
-    print("Sum of total expeses after Transofrmation :", df['Betrag'].sum())
+    print("Sum of total expeses after Transofrmation :", df['amount'].sum())
     return None
 
 if __name__ == "__main__":
